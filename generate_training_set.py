0118b0246f67a91b569212021518af03f489fb09
singurty <singurty@gmail.com>
2022-10-26 18:02:31 +0545

    dateset nice

--- generate_training_set.py
+++ generate_training_set.py
@@ -5,16 +5,28 @@ import chess.pgn
 import os
 from state import State
 
-for fn in os.listdir("data"):
-    pgn = open(os.path.join("data", fn))
-    while 1:
-        try:
-            game = chess.pgn.read_game(pgn)
-        except Exception:
-            break
-        value = {'1/2-1/2':0, '1-0':1, '0-1':-1}[game.headers['Result']]
-        print(value)
-        board = game.board()
-        for move in game.mainline_moves():
-            board.push(move)
-            print(value, State(board).serialize()[0, :, :])
+def get_dateset(num_samples=None):
+    X, Y = [], []
+    games_count = 0
+    for fn in os.listdir("data"):
+        pgn = open(os.path.join("data", fn))
+        while 1:
+            try:
+                game = chess.pgn.read_game(pgn)
+            except Exception:
+                break
+            print("parsing game {}, got {} examples".format(games_count, len(X)))
+            games_count += 1
+            value = {'1/2-1/2':0, '1-0':1, '0-1':-1}[game.headers['Result']]
+            board = game.board()
+            for move in game.mainline_moves():
+                board.push(move)
+                ser = State(board).serialize()[0, :, :]
+                X.append(ser)
+                Y.append(value)
+            if num_samples is not None and len(X) > num_samples:
+                return X, Y
+    return X, Y
+
+if __name__ == "__main__":
+    X, Y = get_dateset(1000)
